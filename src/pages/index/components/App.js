303da65fc416e9511db9189c72ab9347be441f3a
renzhao <renzhao@vpgame.cn>
2017-06-30 13:07:44 +0800

    修改样式

--- src/pages/index/components/App.js
+++ src/pages/index/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { Button, NavBar, Icon } from 'antd-mobile'
+import { Button, NavBar, Icon, Card, WingBlank, WhiteSpace, List, SearchBar } from 'antd-mobile'
 
 import './style.less'
 import Tab from './tab'
+
+const Item = List.Item
 
 class App extends Component {
 
@@ -30,21 +32,50 @@ class App extends Component {
     return (
       <div>
         <NavBar leftContent="back"
-          mode="light"
+          mode="dark"
           onLeftClick={() => console.log('onLeftClick')}
           rightContent={[
-            <Icon key="0" type="search" style={{ marginRight: '0.32rem' }} />,
+            <Icon
+              key="0"
+              type="search"
+              style={{ marginRight: '0.32rem' }}
+            />,
             <Icon key="1" type="ellipsis" />,
           ]}
         >NavBar</NavBar>
-        {this.state.number}
-        <Button className="btn" type="primary" onClick={this.handleAdd}>add</Button>
-        <img className="my-img" src={require('../../../assets/avatar.png')} alt="" />
-        {this.state.data.map((v, i) => {
-          return <div key={i}>{v.name}</div>
-        })}
+        <div className="content">
+          <SearchBar
+            placeholder="手动获取获取光标"
+          />
 
-        <Tab />
+          <Tab />
+          <Card full={false}>
+            <Card.Header
+              title="This is title"
+              thumb={require('../../../assets/avatar.png')}
+              thumbStyle={{ width: 200 }}
+              extra={<span>{this.state.number}</span>}
+            />
+            <Card.Body>
+              <div>This is content of `Card`</div>
+            </Card.Body>
+          </Card>
+          <WhiteSpace size="lg" />
+          <WingBlank>
+            <Button className="btn" type="primary" onClick={this.handleAdd}>add</Button>
+          </WingBlank>
+          <WhiteSpace size="lg" />
+          <List>
+            {this.state.data.map((v, i) => {
+              return (
+                <Item key={i}>
+                  <div>{v.name}</div>
+                </Item>
+              )
+            })}
+          </List>
+
+        </div>
       </div>
     )
   }
